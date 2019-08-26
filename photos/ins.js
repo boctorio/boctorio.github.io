28e65a8758b90067b83edea651226a908b0465d7
boctorio <1614466758@qq.com>
2019-08-27 00:02:57 +0800

    Site updated: 2019-08-27 00:02:18

--- photos/ins.js
+++ photos/ins.js
@@ -116,8 +116,8 @@
           var data = res.list[j].arr;
           var liTmpl = "";
           for (var i = 0, len = data.link.length; i < len; i++) {
-            var minSrc = 'https://github.com/boctorio/photo_hub/tree/master/min_photos/' + data.link[i];
-            var src =    'https://github.com/boctorio/photo_hub/tree/master/photos/' + data.link[i];
+            var minSrc = 'https://raw.githubusercontent.com/boctorio/photo_hub/master/min_photos/' + data.link[i];
+            var src =    'https://raw.githubusercontent.com/boctorio/photo_hub/master/photos/' + data.link[i];
             var type = data.type[i];
             var target = src + (type === 'video' ? '.mp4' : '.jpg');
             src += '';
