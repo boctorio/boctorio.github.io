2160713e4d7694cd80a9d156fbac427434f483f7
boctorio <1614466758@qq.com>
2019-10-09 20:33:36 +0800

    Site updated: 2019-10-09 20:32:57

--- js/src/post-details.js
+++ js/src/post-details.js
@@ -1,4 +1,12 @@
 /* global NexT: true */
+$(document).ready(function(){
+    $(document).on('click', '.fold_hider', function(){
+        $('>.fold', this.parentNode).slideToggle();
+        $('>:first', this).toggleClass('open');
+    });
+    //默认情况下折叠
+    $("div.fold").css("display","none");
+});
 
 $(document).ready(function () {
 
